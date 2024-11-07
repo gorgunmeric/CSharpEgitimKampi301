77bf59054c897dffbc8c03dd0161b0cfea0a1439
gorgunmeric <156701086+gorgunmeric@users.noreply.github.com>
2024-11-07 17:48:51 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
+
+
+
+//int x;   //field
+//public int y { get; set; }  //property
+//void test() //Değişken
+//{
+//    int z;
+//}
